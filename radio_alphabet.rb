1af783afe3ac73adb32c9f5c471aff9774397fdc
tekniklr <tekniklr@tekniklr.com>
2012-02-10 18:07:02 -0800

    Added a clunky scoring system.

--- radio_alphabet.rb
+++ radio_alphabet.rb
@@ -30,11 +30,9 @@ alphabet = {
 }
 
 letters = alphabet.keys.shuffle
-
 completed = 0
 failures = 0
 aborted = false
-
 start_time = Time.now
 
 begin
@@ -61,6 +59,51 @@ rescue Interrupt
 end
 
 unless aborted
+  # only save a score if they didn't quit
+  
   time_expended = (Time.now - start_time).to_i
   puts "\nYou answered all letters in #{time_expended} seconds with #{failures} #{failures == 1 ? 'error' : 'errors'}."
+  
+  # save score and find top score- append line to file of the format 
+  # "unixtimestamp solvetime errors"
+  now = "#{Time.now.to_i} #{time_expended} #{failures}"
+  best = "0 9999 9999" # really, worst
+  
+  # initialize score file, find the best score in it, add the current 
+  # score to it
+  filename = '.radio_albhabet'
+  if !File.exists?(filename)
+    require 'fileutils'
+    FileUtils.touch(filename)
+  end
+  scorefile = File.open(filename, 'r+') do |file|
+    file.flock File::LOCK_EX
+    lines = file.readlines
+    lines.each do |line|
+      # compare the current best with this line, if this line is better
+      # (less time with less errors) then it becomes the new best
+      best_bits = best.split(' ')
+      these_bits = line.split(' ')
+      if (these_bits[1].to_i < best_bits[1].to_i) || ((these_bits[1].to_i == best_bits[1].to_i) && (these_bits[2].to_i < best_bits[2].to_i))
+        # this line is the new best, copy its value
+        best = String.new(line)
+      end
+    end
+    # add the current score
+    file.print "#{now}\n"
+  end
+  # is the current score better than any previous score?
+  best_bits = best.split(' ')
+  if (time_expended.to_i < best_bits[1].to_i) || ((time_expended.to_i == best_bits[1].to_i) && (failures.to_i < best_bits[2].to_i))
+    best = String.new(now)
+  end
+  
+  # report high score
+  if best == now
+    puts "Congratulations, this is your new fastest time."
+  else
+    best_bits = best.split(' ')
+    puts "Your best time is #{best_bits[1]} seconds with #{best_bits[2]} #{best_bits[2] == 1 ? 'error' : 'errors'} on #{Time.at(best_bits[0].to_i).strftime('%B %e, %Y @ %H:%M')}"
+  end
+  
 end
